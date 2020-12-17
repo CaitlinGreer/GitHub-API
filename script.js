e1e85b836d451bd7e8d8fd06ff2c1b25b2690b78
Caitlin Greer <clento1121@gmail.com>
2020-12-17 16:21:37 +0000

    added catch error

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 'use strict';
 
-const url="https://api.github.com/users/"
-
-function watchForm(){
+function watchFormSubmit(){
   $('.search-form').submit(event => {
     event.preventDefault();
     $(".js-results-list").empty();
-    let userSearchInput = $(".username-input").val();
-    let 
-  })
+    const userName = $('.username-search').val();
+    getUsers(userName);
+  });
 }
 
-function getUsers(url){
-  
+function getUsers(userName){
+const url="https://api.github.com/users/" + userName + "/repos"
+
+  fetch(url)
+  .then(response => response.json())
+  .then(responseJson => showRepos(responseJson))
+  .catch(error => alert("Oops!, something went wrong!"));
+
+
 }
+
+function showRepos(responseJson){
+  console.log(responseJson);
+  for (let i = 0; i < responseJson.length; i++){
+    $('.js-results-list').append(`<li><h3><a href="${responseJson[i].html_url}">${responseJson[i].name}</a></h3></li>`
+    )};
+
+    $('.js-results').removeclass('hidden');
+}
+
+
+$(watchFormSubmit);
