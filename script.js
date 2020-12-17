d3b7d4df102a46b85aeb970187f362ec4417450d
Caitlin Greer <clento1121@gmail.com>
2020-12-17 15:21:29 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const url="https://api.github.com/users/"
+
+function watchForm(){
+  $('.search-form').submit(event => {
+    event.preventDefault();
+    $(".js-results-list").empty();
+    let userSearchInput = $(".username-input").val();
+    let 
+  })
+}
+
+function getUsers(url){
+  
+}
